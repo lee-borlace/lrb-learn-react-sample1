d9644666bcadc8410ef4e9f17ad61ce20169a2d9
leeroy79 <lee.borlace@gmail.com>
2016-11-17 18:01:24 +0800

    Added some fun code to play with!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
@@ -11,7 +13,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get bloody started, edit <code>src/App.js</code> and save to reload!!!
+          To get bloody started, edit <code>src/App.js</code> and save to reload!!!!!!
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,67 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// Update this depending on the code you want to test.
+startUp3();
+
+function startUp1() {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+function startUp2() {
+  const element = <h1>Hello, world</h1>;
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+function startUp3() {
+  function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  }
+
+  setInterval(tick, 1000);
+}
+
+function startUp4() {
+}
+
+function startUp5() {
+}
+
+function startUp6() {
+}
+
+function startUp7() {
+}
+
+function startUp8() {
+}
+
+function startUp9() {
+}
+
+function startUp10() {
+}
+
+function startUp11() {
+}
+
+function startUp12() {
+}
+
+function startUp13() {
+}
