6020526e13f1eab6d66c06e8e7812eb7dbd23f30
leeroy79 <lee.borlace@gmail.com>
2016-11-17 21:18:06 +0800

    Further examples.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 
 // Update this depending on the code you want to test.
-startUp3();
+startUp6();
 
 function startUp1() {
   ReactDOM.render(
@@ -14,13 +14,14 @@ function startUp1() {
 }
 
 function startUp2() {
-  const element = <h1>Hello, world</h1>;
+  const element = <h1>Helloooo, world</h1>;
   ReactDOM.render(
     element,
     document.getElementById('root')
   );
 }
 
+// Every time you call render again it only updates the parts of the DOM which have actually changed.
 function startUp3() {
   function tick() {
     const element = (
@@ -29,6 +30,7 @@ function startUp3() {
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
     );
+
     ReactDOM.render(
       element,
       document.getElementById('root')
@@ -38,13 +40,109 @@ function startUp3() {
   setInterval(tick, 1000);
 }
 
+// Any attributes passed to a custom component are consumed via the props arg. 
 function startUp4() {
+
+  function MyComponent(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+
+  const element = <MyComponent name="John Johnson" />;
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
 }
 
+
+// Components can be composed from other components.
 function startUp5() {
+
+  function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+
+  function App3() {
+    return (
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+      </div>
+    );
+  }
+
+  ReactDOM.render(
+    <App3 />,
+    document.getElementById('root')
+  );
 }
 
+
+
+// Example of breaking down a component into lots of sub-components, each with a relatively small scope, and being more re-usable. 
+// We recommend naming props from the component's own point of view rather than the context in which it is being used.
 function startUp6() {
+
+  function Avatar(props) {
+    return (
+      <img className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+        />
+    );
+  }
+
+
+  function UserInfo(props) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
+        </div>
+      </div>
+    );
+  }
+
+
+  function Comment(props) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    );
+  }
+
+  function formatDate(date) {
+    return date.toLocaleDateString();
+  }
+
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+  };
+  
+  
+  ReactDOM.render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author} />,
+    document.getElementById('root')
+  );
+
 }
 
 function startUp7() {
