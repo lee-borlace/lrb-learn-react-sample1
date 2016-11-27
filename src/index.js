458ac73f3d9678ec40289d170d9f84fc5d329059
leeroy79 <lee.borlace@gmail.com>
2016-11-27 10:50:40 +0800

    .

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import Calculator3 from './Calculator3';
 // This code comes from https://facebook.github.io/react/docs/installation.html
 
 // Update this depending on the code you want to test.
-startUp19();
+startUp23();
 
 function startUp1() {
   ReactDOM.render(
@@ -328,16 +328,169 @@ function startUp19() {
   );
 }
 
+// The special "children" prop allows you to just nest further JSX inside the outer component when rendering.
 function startUp20() {
+  function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+
+  function WelcomeDialog() {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          Welcome
+      </h1>
+        <p className="Dialog-message">
+          Thank you for visiting our spacecraft!
+      </p>
+      </FancyBorder>
+    );
+  }
+
+  ReactDOM.render(
+    <WelcomeDialog />,
+    document.getElementById('root')
+  );
 }
 
+// If you want more than one "hole" to fill, then you can define custom ones as in this partial example.
 function startUp21() {
+
+  function SplitPane(props) {
+    return (
+      <div className="SplitPane">
+        <div className="SplitPane-left">
+          {props.left}
+        </div>
+        <div className="SplitPane-right">
+          {props.right}
+        </div>
+      </div>
+    );
+  }
+
+
+  // function App() {
+  //   return (
+  //     <SplitPane
+  //       left={
+  //         <Contacts />
+  //       }
+  //       right={
+  //         <Chat />
+  //       } />
+  //   );
+  // }
+
+
 }
 
+// A more specialised component calling a less specialised one.
 function startUp22() {
+
+  function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+
+
+  function Dialog(props) {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          {props.title}
+        </h1>
+        <p className="Dialog-message">
+          {props.message}
+        </p>
+      </FancyBorder>
+    );
+  }
+
+  function WelcomeDialog() {
+    return (
+      <Dialog
+        title="Welcome"
+        message="Thank you for visiting our spacecraft!" />
+    );
+  }
+
+  ReactDOM.render(
+    <WelcomeDialog />,
+    document.getElementById('root')
+  );
+
+
 }
 
+// Same concept but using a class-based component.
 function startUp23() {
+
+  function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+
+
+  function Dialog(props) {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          {props.title}
+        </h1>
+        <p className="Dialog-message">
+          {props.message}
+        </p>
+        {props.children}
+      </FancyBorder>
+    );
+  }
+
+  class SignUpDialog extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSignUp = this.handleSignUp.bind(this);
+      this.state = { login: '' };
+    }
+
+    render() {
+      return (
+        <Dialog title="Mars Exploration Program"
+          message="How should we refer to you?">
+          <input value={this.state.login}
+            onChange={this.handleChange} />
+          <button onClick={this.handleSignUp}>
+            Sign Me Up!
+          </button>
+        </Dialog>
+      );
+    }
+
+    handleChange(e) {
+      this.setState({ login: e.target.value });
+    }
+
+    handleSignUp() {
+      alert(`Welcome aboard, ${this.state.login}!`);
+    }
+  }
+
+  ReactDOM.render(
+    <SignUpDialog />,
+    document.getElementById('root')
+  );
+
 }
 
 function startUp24() {
